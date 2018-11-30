aa50a7e37d3953c17db5efbb5d0c00b80f79c532
Alexandre Picquot <apicquot@jefferies.com>
2018-11-30 15:03:22 -0500

    some changes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace testdotnet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("need pull request now");
+            Console.WriteLine("need pull request");
         }
     }
 }
